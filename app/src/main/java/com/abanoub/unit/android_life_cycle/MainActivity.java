c6856ca1e9dea3a673bae1bf6568535b9120cfa1
abanoub ghebreal <abanoub.eskander15@gmail.com>
2018-08-16 17:24:10 +0200

    Step NO.1: Create and prepare the project

---
+++ app/src/main/java/com/abanoub/unit/android_life_cycle/MainActivity.java
@@ -0,0 +1,59 @@
+package com.abanoub.unit.android_life_cycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+     * This TAG_LOG will be used for logging. It is best practice to use the class's name using
+     * getSimpleName as that will greatly help to identify the location from which your logs are
+     * being posted.
+     */
+    private static final String TAG_LOG = MainActivity.class.getSimpleName();
+
+    /* Constant values for the names of each respective lifecycle callback */
+    private static final String ON_CREATE = "onCreate";
+    private static final String ON_START = "onStart";
+    private static final String ON_RESUME = "onResume";
+    private static final String ON_PAUSE = "onPause";
+    private static final String ON_STOP = "onStop";
+    private static final String ON_RESTART = "onRestart";
+    private static final String ON_DESTROY = "onDestroy";
+    private static final String ON_SAVE_INSTANCE_STATE = "onSaveInstanceState";
+
+    /*
+     * This TextView will contain a running log of every lifecycle callback method called from this
+     * Activity. This TextView can be reset to its default state by clicking the Button labeled
+     * "Reset Log"
+     */
+    private TextView mLifecycleDisplay;
+
+    /**
+     * Called when the activity is first created. This is where you should do all of your normal
+     * static set up: create views, bind data to lists, etc.
+     *
+     * Always followed by onStart().
+     *
+     * @param savedInstanceState The Activity's previously frozen state, if there was one.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLifecycleDisplay = findViewById(R.id.tv_lifecycle_display);
+    }
+
+
+    /**
+     * This method resets the contents of the TextView to its default text of "Lifecycle callbacks"
+     *
+     * @param view The View that was clicked. In this case, it is the Button from our layout.
+     */
+    public void resetLifecycleDisplay(View view){
+        mLifecycleDisplay.setText("Lifecycle callbacks:\n");
+    }
+}
